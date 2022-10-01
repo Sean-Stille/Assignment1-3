c465c09accec32d62b4ce51d457155c11af6b4f7
Sean Stille <seanstille0627@gmail.com>
2022-10-01 16:37:33 -0500

    Added comments

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        FileReader setup = new FileReader(100);
+        FileReader setup = new FileReader(100); //FileReader class demo
         
     }
 }
--- src/FileReader.java
+++ src/FileReader.java
@@ -5,19 +5,25 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.random.*;
 /**
- * 
+ * FileReader class, essentially takes in an int, which specifies how many elements you want in input4.txt, and then
+ * randomly generates them, choosing from the list of tree names which needs to be entered in "treeNames.txt"
  */
 public class FileReader {
     final String INPUT_4 = "input4.txt";
     final String TREE_FILE = "treeNames.txt";
+    /**
+     * Constructor, takes in the number of elements and generates them
+     * @param numElements The number of elements you want input4.txt to have
+     */
     public FileReader(int numElements){
         generateList(numElements);
     }
 
     /**
-     * 
-     * @param numElements 
-     * @return
+     * This is the method that actually does all the work. it is called when the class is first instantiated,
+     * it can also be called after that to regenerate a new random list, in case we want to do unit testing.
+     * @param numElements The number of elements you want input4.txt to have
+     * @return boolean to show it worked, I didn't really implement this though.
      */
     public boolean generateList(int numElements){
         LinkedList<String> list = new LinkedList<>();
@@ -43,7 +49,7 @@ public class FileReader {
             scan.close();
             write.close();     
         }
-        return false;
+        return true;
     }
 
 
